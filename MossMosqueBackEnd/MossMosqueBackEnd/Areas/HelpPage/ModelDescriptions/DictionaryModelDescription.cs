ef93bf3e12439b5816b8f0e7ef5c6439c1bffdb4
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2017-03-06 08:53:30 +0100

    set up the project

---
+++ MossMosqueBackEnd/MossMosqueBackEnd/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MossMosqueBackEnd.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MossMosqueBackEnd/MossMosqueBackEnd/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MossMosqueBackEnd.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
