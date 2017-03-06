2c0504b311103f9cccf1ac2e7ba56d0bafa10146
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2017-03-06 10:18:03 +0100

    added query to insert into date

---
+++ MossMosqueBackEnd/Database/Tables/Dates.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Tables
+{
+    public class Dates
+    {
+        public DBConnect DB;
+
+        public Dates()
+        {
+            DB = new DBConnect();
+        }
+
+        public void Insert()
+        {
+            //open connection
+            if (DB.OpenConnection() == true)
+            {
+                using (var command = DB.GetConnection().CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO dates (date) VALUES (@1)";
+                    command.Parameters.AddWithValue("@1", DateTime.Now.ToString("yyyy-MM-dd"));
+                    command.ExecuteNonQuery();
+                }
+                //close connection
+                DB.CloseConnection();
+            }
+        }
+    }
+}
--- MossMosqueBackEnd/MossMosqueBackEnd/Controllers/HomeController.cs
+++ MossMosqueBackEnd/MossMosqueBackEnd/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Database.Tables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,10 @@ namespace MossMosqueBackEnd.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            Dates d = new Dates();
+            d.Insert();
+
 
             return View();
         }
